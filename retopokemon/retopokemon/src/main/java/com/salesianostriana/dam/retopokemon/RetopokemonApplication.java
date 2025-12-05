b1addeeb9ca93d6bd1f5150ce460f10fa2c4e358
Carmen Fernández de Córdoba <carmenfdcpwork@gmail.com>
2025-12-05 09:58:51 +0100

    retoPokemon

---
+++ retopokemon/retopokemon/src/main/java/com/salesianostriana/dam/retopokemon/RetopokemonApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.retopokemon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetopokemonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetopokemonApplication.class, args);
+	}
+
+}
---
+++ retopokemon/retopokemon/src/test/java/com/salesianostriana/dam/retopokemon/RetopokemonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.retopokemon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetopokemonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
