2a1e38d2a943db4d206208b7a4dd612a90eb3278
Carmen Fernández de Córdoba <carmenfdcpwork@gmail.com>
2025-12-16 12:31:32 +0100

    Import en monumentos

--- monumentos/monumentos/src/main/java/com/salesianostriana/dam/monumentos/controlador/MonumentsController.java
+++ monumentos/monumentos/src/main/java/com/salesianostriana/dam/monumentos/controlador/MonumentsController.java
@@ -1,6 +1,9 @@
 package com.salesianostriana.dam.monumentos.controlador;
 import java.util.List;
 import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
